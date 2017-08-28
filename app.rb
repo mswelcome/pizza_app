5dc21929314ff2a6944de32405d2dfdba5a7b381
mswelcome <mattwelcome@gmail.com>
2017-08-28 07:21:50 -0400

    Nothing freaking works

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@
 require "sinatra"
 enable :sessions
 
-@cost = 0
+
 
 get '/' do
 
@@ -12,46 +12,59 @@ get '/' do
 end
 
 post '/select' do
-
+  $cost = 0
   session[:meats] = params[:meats]
   session[:veggies] = params[:veggies]
   session[:cheese] = params[:cheese]
   session[:size] = params[:size]
-  session[:delivery] = params[:delivery]
-  if size == "extralarge"
-    @cost = @cost + 15
-  elsif size == "large"
-    @cost = @cost + 12
-  elsif size == "medium"
-    @cost = @cost + 9
-  else size == "small"
-    @cost = @cost + 7
+
+  if session[:size] == 'extralarge'
+    $cost = $cost + 15
+  elsif session[:size] == 'large'
+    $cost = $cost + 12
+  elsif session[:size] == 'medium'
+    $cost = $cost + 9
+  else session[:size] == 'small'
+    $cost = $cost + 7
   end
+
+  session[:total] = $cost
+  session[:delivery] = params[:delivery]
+
   redirect '/confirm?'
 
 end
 
 get '/confirm' do
 
-  erb :confirm, locals: {meats: session[:meats], veggies: session[:veggies], cheese: session[:cheese], size: session[:size], delivery: session[:delivery]}
+  erb :confirm, locals: {total: session[:total], meats: session[:meats], veggies: session[:veggies], cheese: session[:cheese], size: session[:size], delivery: session[:delivery]}
 
 end
 
 post '/res' do
-
   session[:address] = params[:address]
   session[:conf] = params[:conf].to_a
-  redirect '/results?'
+  redirect '/more?'
 
 end
 
 get '/more' do
-    session[:pizzas] = params[:pizzas]
+
+
     erb :more, locals: {conf: session[:conf], address: session[:address], size: session[:size]}
 
 end
 
-get 'results' do
+post '/final' do
+  session[:pies] = params[:pies]
+  @params = params
+  print @params
+  redirect  '/results?'
+end
 
-    erb :results, locals: {}
+
+
+get '/results' do
+    session[:pies] = params[:pies]
+    erb :results, locals: {pizzas: session[:pizzas]}
 end
